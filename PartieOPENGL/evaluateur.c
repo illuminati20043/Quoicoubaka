2ebbd1f759d330776a230a78a0c43ab3d03ac9dd
illuminati20043 <53662765+illuminati20043@users.noreply.github.com>
2024-03-27 14:07:01 +0100

    Update evaluateur.c
    lots, lots, lots of mistakes

--- PartieOPENGL/evaluateur.c
+++ PartieOPENGL/evaluateur.c
@@ -41,28 +41,28 @@ float Evaluateur(Node* A, float X)
 	case FONCTION:
 		switch (A->jeton.valeur.fonction) {
 		case SIN:
-			return sin(Evaluateur(A->pjeton_preced), X);
+			return sin(Evaluateur(A->pjeton_preced, X));
 			break;
 		case COS:
-			return cosf(Evaluateur(A->pjeton_preced), X);
+			return cosf(Evaluateur(A->pjeton_preced, X));
 			break;
 		case TAN:
-			return tanf(Evaluateur(A->pjeton_preced), X);
+			return tanf(Evaluateur(A->pjeton_preced, X));
 			break;
 		case EXP:
-			return expf(Evaluateur(A->pjeton_preced), X);
+			return expf(Evaluateur(A->pjeton_preced, X));
 			break;
 		case LOG:
-			return logf(Evaluateur(A->pjeton_preced), X);
+			return logf(Evaluateur(A->pjeton_preced, X));
 			break;
 		case ENTIER:
-			return floor(Evaluateur(A->pjeton_preced), X);
+			return floor(Evaluateur(A->pjeton_preced, X));
 			break;
 		case VAL_NEG:
-			return -abs(Evaluateur(A->pjeton_preced), X);
+			return -abs(Evaluateur(A->pjeton_preced, X));
 			break;
 		case SINC:
-			return sin(Evaluateur(A->pjeton_preced), X);
+			return sinf(Evaluateur(A->pjeton_preced, X));
 			break;
 		default:
 			printf("Bad function");
@@ -77,36 +77,9 @@ float Evaluateur(Node* A, float X)
 
 }
 
+int main(){
 
-float* Eval_Arbre_Range_x(struct Node* Arbre, float x_min, float x_max, int n) {
-	float step = (x_max - x_min) / (n - 1);
-	float x_temp;
-	float* resultsx = malloc(n * sizeof(float));
-
-	for (int i = 0; i < n; i++) {
-		x_temp = x_min + i * step;
-		resultsx[i] = x_temp;
-	}
-	return resultsx;
-}
-float* Eval_Arbre_Range_y(struct Node* Arbre, float x_min, float x_max, int n) {
-	float step = (x_max - x_min) / (n - 1);
-	float x_temp;
-	float y_temp;
-	float* resultsy = malloc(n * sizeof(float));
-
-	for (int i = 0; i < n; i++) {
-		x_temp = x_min + i * step;
-		y_temp = Parcourir(Arbre, x_temp);
-		resultsy[i] = y_temp;
-	}
-	return resultsy;
-}
-
-
-
-
-Node Node8 = { {REEL,{.reel = 3}},NULL,NULL };
+	Node Node8 = { {REEL,{.reel = 3}},NULL,NULL };
 Node Node7 = { {VARIABLE,{.reel = 0}},NULL,NULL };
 Node Node6 = { {OPERATEUR,{.operateur = PLUS}},&Node7,&Node8 };
 Node Node5 = { {FONCTION,{.fonction = SIN}},&Node6,NULL };
@@ -114,3 +87,12 @@ Node Node4 = { {REEL,{.reel = 6}},NULL,NULL };
 Node Node3 = { {OPERATEUR,{.operateur = FOIS}},&Node4,&Node5 };
 Node Node2 = { {REEL,{.reel = 4}},NULL,NULL };
 Node Node1 = { {OPERATEUR,{.operateur = PLUS}},&Node2,&Node3 };
+
+	float a = Evaluateur(&Node1,1);
+	printf("%f \n",a);
+	
+
+
+
+}
+
